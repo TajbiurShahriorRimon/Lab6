fae84ea5ebff24fbb882294b1d267df779346385
Rimon <tajbiurshahriorrimon@gmail.com>
2020-11-10 16:52:14 +0600

    3rd push

--- Addbook.php
+++ Addbook.php
@@ -1,7 +1,7 @@
 <?php
-/*if(!isset($_COOKIE['user_Name'])){
+if(!isset($_COOKIE['user_Name'])){
     header("Location: Login.php");
-}*/
+}
 echo "Books";
 ?>
 <html>
@@ -14,7 +14,37 @@ echo "Books";
         <td>Book Name:</td>
     </tr>
     <tr>
-        <td><textarea rows="3" cols="5" ></textarea></td>
+        <td><textarea rows="3" cols="10" ></textarea></td>
+    </tr>
+    <tr>
+        <td>Publisher:</td>
+    </tr>
+    <tr>
+        <td><input type="text" width="5"></textarea></td>
+    </tr>
+    <tr>
+        <td>Edition:</td>
+    </tr>
+    <tr>
+        <td><input type="text" width="5"></textarea></td>
+    </tr>
+    <tr>
+        <td>ISBN:</td>
+    </tr>
+    <tr>
+        <td><input type="text" width="5"></textarea></td>
+    </tr>
+    <tr>
+        <td>Pages:</td>
+    </tr>
+    <tr>
+        <td><input type="text" width="5"></textarea></td>
+    </tr>
+    <tr>
+        <td>Price:</td>
+    </tr>
+    <tr>
+        <td><input type="text" width="5"></textarea></td>
     </tr>
 </table>
 </body>
